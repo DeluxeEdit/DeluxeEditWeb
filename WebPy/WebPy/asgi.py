6f49ecb6f4ff1e66369a88bbf5f5c620e1d67348
Pierre Axelsson <pierre.axelsson@outlook.com>
2025-06-19 20:28:36 +0200

    Added GetFiles

--- WebPy/WebPy/asgi.py
+++ WebPy/WebPy/asgi.py
@@ -14,3 +14,25 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WebPy.settings')
 
 application = get_asgi_application()
+pathToInstallFiles='/mnt/c/Slask'
+installFileExtension='.msix'
+
+def    GetFiles(path, name)
+    result=[]
+
+       
+
+    for root, dir, files in os.walk(path)
+
+                                         
+        if name in files:
+            result.append(os.path.join(root, filename))
+return  result
+
+
+
+
+
+
+
+
