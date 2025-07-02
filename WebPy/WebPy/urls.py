62e30bfdd2250c51aa08dc4d7ea8a235226b050c
Pierre Axelsson <pierre.axelsson@outlook.com>
2025-07-02 20:07:52 +0200

    Added views import

--- WebPy/WebPy/urls.py
+++ WebPy/WebPy/urls.py
@@ -14,9 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
 
+from . import views
 admin.site.site_header = 'Offical Web Site for DeluxeEdit advanced text editor'
 admin.site.site_title = 'Offical Web Site for DeluxeEdit advanced text editor'
 
