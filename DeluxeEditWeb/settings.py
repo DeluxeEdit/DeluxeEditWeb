47fa411af79e8b84c3dba110c6b52bc5311fcb90
Pierre Axelsson <pierre.axelsson@outlook.com>
2025-08-28 13:18:16 +0200

    Added django as vs project

--- DeluxeEditWeb/settings.py
+++ DeluxeEditWeb/settings.py
@@ -12,12 +12,12 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
-
+# PROJECT_DIR="."
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = "." #Path(__file__).resolve().parent.parent
 
 PathOnPAN=r"c:\Slask"
-PathOnPI=' '
+PathOnPI=""
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
